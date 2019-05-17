0de9b45ed1b61d1198d67d6b51524eb9e0f866f7
Lynx <1197225628@qq.com>
2019-05-17 16:06:12 +0800

    标题 function

--- md_parser/src/main.cpp
+++ md_parser/src/main.cpp
@@ -40,11 +40,12 @@ void openFile()
   }
 }
 
+char line[512];// 每一行
+char render[1024];// 解析后的字符串
+
 void readFile()
 {
   int i = 0;
-  char line[512];// 每一行
-  char render[1024];// 解析后的字符串
   int title = 0;// 标题级数
   int tag = 0;// 缩进级数
   int tagStack[8]; // 最多8种缩进
@@ -66,46 +67,47 @@ void readFile()
 
     if (length == 0) continue;// 空行
 
-    if (line[0] != ' ') {// 非缩进
-      title = 0;
-      for (i = 0; line[i] == '#'; i ++) {// 标题
-        title ++;// 标题级数增加,TODO 最大标题级数
-      }
-      if (line[i] != ' ') {// 判断语法,#后接空格,TODO
-        title = 0;// 不合语法
-      } else {
-        i ++;// 空格
-      }
-    } else {// 缩进
-      for (i = 0; line[i] == ' '; i ++) {
-        tag ++;// 缩进级数增加
-      }
-      if (line[i] != ' ') {// 语法判断
-        tag = 0;// 不合语法
-      } else {
-        i ++;// 空格
-        if (tag > tagStack[tagStackTop]) {// 更小一级
-          assert(tagStackTop < 8);
-          tagStack[tagStackTop] = tag;
-          tagStackTop ++;
-          assert(tagStack[tagStackTop] == -1);
-        } else if (tag == tagStack[tagStackTop]) {// 同级
-          assert(tag != -1);
-        } else {// 向前回溯
-          ;
-        }
-      }
+    if (line[0] == '#') {// 标题,标题前不能有空格,#后要有空格
+      isTitle();
     }
-    if (title) {// 是标题
-      tag = 0;// 清空tag
-      memset(render, 0, sizeof(render));
-      sprintf(render, "<h%d>%s</h%d>\n", title, line + i, title);
-      MAGENTA("%s", render);
-      // fputs(render, html);
-    } else if (tag) {// 有缩进
-      ;
+
+    for (i = 0; line[i] == ' '; i ++) {
+      tag ++;// 缩进级数增加
+    }
+    if (line[i] != ' ') {// 语法判断
+      tag = 0;// 不合语法
+    } else {
+      i ++;// 空格
+      if (tag > tagStack[tagStackTop]) {// 更小一级
+        assert(tagStackTop < 8);
+        tagStack[tagStackTop] = tag;
+        tagStackTop ++;
+        assert(tagStack[tagStackTop] == -1);
+      } else if (tag == tagStack[tagStackTop]) {// 同级
+        assert(tag != -1);
+      } else {// 向前回溯
+        ;
+      }
     }
   }
 
   return;
+}
+
+void isTitle()// 标题
+{
+  int i = 0;
+  for (i = 0; line[i] == '#'; i ++) {
+    title ++;// 标题级数增加,TODO 最大标题级数
+  }
+  if (line[i] == ' ') {// 判断语法,#后接空格,TODO
+    i ++;// 空格
+    tag = 0;// 清空tag
+    memset(render, 0, sizeof(render));
+    sprintf(render, "<h%d>%s</h%d>\n", title, line + i, title);
+    MAGENTA("%s", render);
+    // fputs(render, html);
+  } else {
+    title = 0;// 不合语法
+  }
 }
