6f92d4079b1104f0cbd8d8b29515d492e95c194a
Lynx <1197225628@qq.com>
2019-05-17 10:09:34 +0800

    打开html

--- md_parser/src/main.cpp
+++ md_parser/src/main.cpp
@@ -2,6 +2,8 @@
 
 char mdFileName[32];
 char htmlFileName[32];
+FILE *md;
+FILE *html;
 
 int main()
 {
@@ -13,18 +15,31 @@ int main()
 void readFileName()
 {
   YELLOW("Your current path: [%s]", getenv("PWD"));
-  GREEN("Input file name");
+
+  GREEN("Input markdown file name");
   scanf("%s", mdFileName);
   green("md filename is ");
   CYAN("%s", mdFileName);
-}
-
-void openFile()
-{
-  FILE *fp = fopen(mdFileName, "r");
-  if (fp == NULL) {
+  md = fopen(mdFileName, "r");
+  if (md == NULL) {
     RED("[%s] doesn't exist", mdFileName);
   } else {
     GREEN("open [%s] successfully", mdFileName);
   }
+
+  GREEN("Input html file name");
+  scanf("%s", htmlFileName);
+  green("html filename is ");
+  CYAN("%s", htmlFileName);
+  html = fopen(htmlFileName, "w+");
+  if (html == NULL) {
+    RED("[%s] open error", htmlFileName);
+  } else {
+    GREEN("open [%s] successfully", htmlFileName);
+  }
+}
+
+void openFile()
+{
+  return;
 }
