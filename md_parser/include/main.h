f641ac579a47ce5e8f5fa8577eaf32ef7963c585
Lynx <1197225628@qq.com>
2019-05-18 17:32:54 +0800

    使用define

--- md_parser/include/main.h
+++ md_parser/include/main.h
@@ -45,10 +45,22 @@
 // 无序/有序表缩进级数
 #define MAX_NAME 32
 // 文件名长度
-#define MAX_CLEAR 64
-// 清除<ul><ol><p>的最大长度
+#define MAX_STR 64
+// 一些特殊字符的长度
+// 如: clear_text, escp_char
 
-// 文件操作
+int my_max(int a, int b)
+{
+  if (a > b) return a;
+  return b;
+}
+
+int my_min(int a, int b)
+{
+  if (a < b) return a;
+  return b;
+}
+
 void openFile();
 void readFile();
 void header();// 添加<html>
@@ -68,33 +80,21 @@ void endLine();// 除去行内环境
 void endTag();// 向前回溯无序表/有序表
 void doEscape(int &i, int &rend_tail);// 正常文本的转义处理
 
-int my_max(int a, int b)
-{
-  if (a > b) return a;
-  return b;
-}
-
-int my_min(int a, int b)
-{
-  if (a < b) return a;
-  return b;
-}
-
-char mdFileName[32];
-char htmlFileName[32];
+char mdFileName[MAX_NAME];
+char htmlFileName[MAX_NAME];
 FILE *md;
 FILE *html;
 
-char line[1024];// 每一行
+char line[MAX_LINE];// 每一行
 int length = 0;// 每一行的长度
-char render[2048];// 解析后的字符串
+char render[MAX_REND];// 解析后的字符串
 
 int tag = -1;// 缩进级数(空格数)
-int tagStack[8]; // 最多8种不同缩进
-int evnStack[8]; // 语言环境栈
+int tagStack[MAX_TAG]; // 最多8种不同缩进
+int evnStack[MAX_TAG]; // 语言环境栈
 // 0: default, 1: title, 2: paragraph, 3: blockquote, 4: ul, 5: ol
 int stackTop = 0;// 语言环境栈顶
-char clear_text[64];// 清除语言环境
+char clear_text[MAX_STR];// 清除语言环境
 int textEvn = 0;// 当前语言环境 
 // 0: plain, 1: title, 2: paragraph, 3: blockquote, 4: ul, 5: ol
 int paraEvn = 0;// 每一段的语言环境
@@ -105,7 +105,7 @@ int codeEvn = 0;
 // 0: none, 1: `code`, 2: ``code``
 
 int escape = 0;// 转义'\'
-char escp_char[32] = "*_\\";
+char escp_char[MAX_STR] = "*_\\";
 int paragraph = 0;// 0: 没有新的段落, 1: 有段落没有结束
 int slide_num = 0;// 当前slide序号,初始0
 
